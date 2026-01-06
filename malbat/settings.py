786311664a3122f8c70dc6367e5227c630ce9756
Victor Tiltay <vtiltay@gmail.com>
2026-01-06 16:43:22 +0100

    fix: cleanup repository by removing ignored .md files

--- malbat/settings.py
+++ malbat/settings.py
@@ -116,7 +116,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME', 'malbat_db'),
         'USER': os.getenv('DB_USER', 'malbat_user'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'Pass123'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '5432'),
     }
